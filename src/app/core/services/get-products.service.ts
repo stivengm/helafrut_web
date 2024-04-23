d86aaa59470821705e68bfe6bb0396dddd6873b8
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-04-23 15:33:00 -0500

    ADD - Se añade servicio para obtener productos.

---
+++ src/app/core/services/get-products.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetProductsService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get(environment.endPoint + `/products`);
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    endPoint: "https://elafrut-fb2ea-default-rtdb.firebaseio.com"
+};
